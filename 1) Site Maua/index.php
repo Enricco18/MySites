72fed95c3f8835c91338b2143032e8de4bd65368
Enricco <enriccoamaral@hotmail.com>
2019-08-12 15:16:48 -0300

    Primeiro site adicionado

---
+++ 1) Site Maua/index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mauá e-Sports</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<header>
+		<img src="img/logo.png" onclick="window.location = 'index.php'; " style="cursor: pointer;">
+		<nav>
+			<a href="#campeonatos" class="menu_campeonatos">campeonatos</a>
+			<a href="#">lineups</a>
+			<a href="index.php">home</a>
+		</nav>
+	</header>
+		<div id="imagem_home"></div>
+		<p class="title_home">Campeonato interno</p>
+
+	<section>
+		<div id="sections">
+			<div class="barra_title"></div>
+			<p class="Title">lineups</p>
+		</div>
+		<div id = "games">
+			<div id="quadradinhosg">
+				<img src="img/Blue_king.png" id="fotosg">
+			</div>
+			<div id="quadradinhosg">
+				<img src="img/Blue_king.png" id="fotosg">
+
+			</div>
+			<div id="quadradinhosg">
+				<img src="img/Blue_king.png" id="fotosg">
+
+			</div>
+			<div id="quadradinhosg">
+				<img src="img/Blue_king.png" id="fotosg">
+
+			</div>
+			<div id="quadradinhosg">
+				<img src="img/Blue_king.png" id="fotosg">
+
+			</div>
+			<div id="quadradinhosg">
+				<img src="img/Blue_king.png" id="fotosg">
+
+			</div>
+			<div id="quadradinhosg">
+				<img src="img/Blue_king.png" id="fotosg">
+
+			</div>
+			<div id="quadradinhosg">
+				<img src="img/Blue_king.png" id="fotosg">
+
+			</div>						
+		</div>
+		<div id="lines">
+			<div id="quadradinhos">
+				<p id="role">ENTRY</p>
+				<p id="nome">Matheus Gamba</p>
+				<p id="nick">"Doom"</p>
+				
+				<img src="img/csgo_model.png" id="fotos">
+			</div>
+			<div id="quadradinhos">
+				<p id="role">ENTRY</p>
+				<p id="nome">Matheus Gamba</p>
+				<p id="nick">"Doom"</p>
+				
+				<img src="img/csgo_model.png" id="fotos">
+			</div>
+			<div id="quadradinhos">
+				<p id="role">ENTRY</p>
+				<p id="nome">Matheus Gamba</p>
+				<p id="nick">"Doom"</p>
+				
+				<img src="img/csgo_model.png" id="fotos">
+			</div>
+			<div id="quadradinhos">
+				<p id="role">ENTRY</p>
+				<p id="nome">Matheus Gamba</p>
+				<p id="nick">"Doom"</p>
+				
+				<img src="img/csgo_model.png" id="fotos">
+			</div>
+			<div id="quadradinhos">
+				<p id="role">ENTRY</p>
+				<p id="nome">Matheus Gamba</p>
+				<p id="nick">"Doom"</p>
+				
+				<img src="img/csgo_model.png" id="fotos">
+			</div>
+			
+		</div>
+	</section>
+</body>
+	
